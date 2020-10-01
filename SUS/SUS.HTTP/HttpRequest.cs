bec7e08ae82daf59d5c14250764a24bda0fe32c5
CodingMania <vasilmitovv@gmail.com>
2020-10-01 17:40:20 +0300

    Extend HttpRequest class

--- SUS/SUS.HTTP/HttpRequest.cs
+++ SUS/SUS.HTTP/HttpRequest.cs
@@ -1,6 +1,9 @@
 ﻿namespace SUS.HTTP
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
     public class HttpRequest
     {
@@ -10,6 +13,50 @@
             this.Headers = new List<Header>();
 
             var lines = requesString.Split(new string[] { HttpConstants.NewLine }, System.StringSplitOptions.None);
+
+            var headerLine = lines[0];
+
+            var headerLineParts = headerLine.Split(' ');
+
+            this.Method = (HttpMethod)Enum.Parse(typeof(HttpMethod), headerLineParts[0]);
+            this.Path = headerLineParts[1];
+
+            var isInHeaders = true;
+            var bodyBuilder = new StringBuilder();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var line = lines[i];
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    isInHeaders = false;
+                    continue;
+                }
+
+                if (isInHeaders)
+                {
+                    this.Headers.Add(new Header(line));
+                }
+                else
+                {
+                    bodyBuilder.AppendLine(line);
+                }
+            }
+
+            if (this.Headers.Any(x => x.Name == HttpConstants.RequestCookieHeader))
+            {
+                var cookiesAsString = this.Headers.FirstOrDefault(x => x.Name == HttpConstants.RequestCookieHeader).Value;
+
+                var cookies = cookiesAsString.Split(new string[] { "; " }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var cookie in cookies)
+                {
+                    this.Cookies.Add(new Cookie(cookie));
+                }
+            }
+
+            this.Body = bodyBuilder.ToString();
         }
 
         public string Path { get; set; }
