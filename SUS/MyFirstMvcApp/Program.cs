79e6575e03ca24fe9783d4285256f57efca94156
CodingMania <vasilmitovv@gmail.com>
2020-10-01 15:54:22 +0300

    Make Main method asynchronous

--- SUS/MyFirstMvcApp/Program.cs
+++ SUS/MyFirstMvcApp/Program.cs
@@ -1,12 +1,24 @@
 ﻿namespace MyFirstMvcApp
 {
+    using System;
+    using System.Threading.Tasks;
+
     using SUS.HTTP;
 
     public class Program
     {
-        public static void Main(string[] args)
+        public async Task Main(string[] args)
         {
             var server = new HttpServer();
+
+            server.AddRoute("/", HomePage);
+
+            await server.StartAsync(80);
+        }
+
+        private static HttpRespose HomePage(HttpRequest arg)
+        {
+            throw new NotImplementedException();
         }
     }
 }
