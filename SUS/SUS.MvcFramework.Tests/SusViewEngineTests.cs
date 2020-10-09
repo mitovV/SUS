d75cb226f011dac1196fca675bdc1c86211517d8
CodingMania <vasilmitovv@gmail.com>
2020-10-09 10:12:11 +0300

    Add tests, viewEngine

---
+++ SUS/SUS.MvcFramework.Tests/SusViewEngineTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SUS.MvcFramework.Tests
+{
+    using System;
+    using System.IO;
+
+    using ViewEngine;
+    using Xunit;
+
+    public class SusViewEngineTests
+    {
+        [Theory]
+        [InlineData("CleanHtml")]
+        [InlineData("Foreach")]
+        [InlineData("IfElseFor")]
+        [InlineData("Model")]
+        public void TestGetHtml(string fileName)
+        {
+
+            var viewModel = new TestViewModel
+            {
+                DateOfBirth = new DateTime(2019, 6, 1),
+                Name = "Doggo Arghentino",
+                Price = 12345.67M,
+            };
+
+            IViewEngine viewEngine = new SusViewEngine();
+            var view = File.ReadAllText($"ViewTests/{fileName}.html");
+            var result = viewEngine.GetHtml(view, viewModel);
+            var expectedResult = File.ReadAllText($"ViewTests/{fileName}.Result.html");
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ SUS/SUS.MvcFramework/ViewEngine/ErrorView.cs
@@ -0,0 +1,10 @@
+﻿namespace SUS.MvcFramework.ViewEngine
+{
+    public class ErrorView : IView
+    {
+        public string ExecuteTemplate(object viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
