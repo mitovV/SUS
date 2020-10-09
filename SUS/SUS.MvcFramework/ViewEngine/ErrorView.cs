ed64cce016e211b19edc8e9b7cdfe385dd0eca83
CodingMania <vasilmitovv@gmail.com>
2020-10-09 10:17:16 +0300

    Implement logic in ErrorView class

--- SUS/SUS.MvcFramework/ViewEngine/ErrorView.cs
+++ SUS/SUS.MvcFramework/ViewEngine/ErrorView.cs
@@ -1,10 +1,31 @@
 ﻿namespace SUS.MvcFramework.ViewEngine
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class ErrorView : IView
     {
+        private readonly IEnumerable<string> errors;
+        private readonly string csharpCode;
+
+        public ErrorView(IEnumerable<string> errors, string csharpCode)
+        {
+            this.errors = errors;
+            this.csharpCode = csharpCode;
+        }
+
         public string ExecuteTemplate(object viewModel)
         {
-            throw new System.NotImplementedException();
+            var html = new StringBuilder();
+            html.AppendLine($"<h1>View compile {this.errors.Count()} errors:</h1><ul>");
+            foreach (var error in this.errors)
+            {
+                html.AppendLine($"<li>{error}</li>");
+            }
+
+            html.AppendLine($"</ul><pre>{csharpCode}</pre>");
+            return html.ToString();
         }
     }
 }
